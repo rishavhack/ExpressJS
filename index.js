fe6d82718f62c9894fc2c4ff09c2454567a7ccaa
rishavhack <rishav.jnit@gmail.com>
2021-09-13 16:51:55 +0530

    Worked on passong data through get api

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const app = express();
 
+const courses = [
+	{id:1,name:'Course1'},
+
+	{id:2,name:'Course2'},
+
+	{id:3,name:'Course3'},
+]
 
 app.get('/',(req,res)=>{
 	res.send("Hello World")
 })
 
 app.get('/api/courses',(req,res)=>{
-	res.send([1,2,3,4])
+	res.send(courses)
 })
 
 app.get('/api/courses/:id',(req,res)=>{
-	res.send(req.params)
+	const course = courses.filter(x=>x.id === parseInt(req.params.id))
+	if(!course.length) res.status(404).send("Not avaiable")
+	res.send(course)
 })
 
 app.get('/api/courses/:year/:month',(req,res)=>{
