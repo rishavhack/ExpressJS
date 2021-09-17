642e31065ad79da4382880fe156ac2b3d36f8d86
rishavhack <rishav.jnit@gmail.com>
2021-09-17 18:41:13 +0530

    Built in Middleware

--- builtinnMiddleware.js
+++ builtinnMiddleware.js
@@ -5,7 +5,7 @@ app.use(express.json())
 
 app.use(logger)
 app.use(express.urlencoded({extended : true}));
-
+app.use(express.static('public'))
 
 app.use(function(req,res,next){
 	console.log('Authenticationn...');
