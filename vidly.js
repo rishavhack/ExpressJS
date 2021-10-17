42fe3b4e4e8e2dee5e88dd9cd6c172b69ba00657
rishavhack <rishav.jnit@gmail.com>
2021-10-17 15:02:12 +0530

    Created Project Vidly

---
+++ vidly.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const genres = [
+	{id:1,name:'Action'},
+	{id:2,name:'Horror'},
+	{id:3,name:'Romance'},
+]
+
+app.get('/api/genres',(req,res) =>  {
+	res.send(genres)
+})
+
+app.listen(port,()=> console.log(`Listening port number ${port}`))
