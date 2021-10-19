94cfa1400ecd9c9ce98c3cd1ea12f3d3eb67ea1c
rishavhack <rishav.jnit@gmail.com>
2021-10-19 15:48:25 +0530

    Created Project Vidly- Update, Delete

--- vidly.js
+++ vidly.js
@@ -13,6 +13,37 @@ app.get('/api/genres',(req,res) =>  {
 	res.send(genres)
 })
 
+app.post('/api/genres',(req,res) =>  {
+	if(Object.keys(req.body).length == 0 ) return res.status(400).send("Invalid Data")
+	if(!req.body.hasOwnProperty('name')) return res.status(400).send("Invalid Data")
+	const genre = {
+		id : genres.length +1,
+		name: req.body.name
+	}
+	genres.push(genre)
+	res.send(genre)
+})
+
+
+
+app.put('/api/genres/:id',(req,res)=>{
+	const genre = genres.find(x => x.id === parseInt(req.params.id))
+	if (!genre) res.status(404).send("Not avaiable ID")
+
+	const index =genres.indexOf(genre)
+	genres[index].name = req.body.name
+	res.send(genre)
+})
+
+
+app.delete('/api/genres/:id',(req,res)=>{
+	const genre = genres.find(x => x.id === parseInt(req.params.id))
+	if (!genre) res.status(404).send("Not avaiable ID")
+
+	const index =genres.indexOf(genre)
+	genres.splice(index,1)
+	res.send(genres)
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=> console.log(`Listening port number ${port}`))
